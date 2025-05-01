8d835d7e7e7e298991340b020a8bd8ec9e098a0e
zeenenglish <zeenenglish@gmail.com>
2025-05-01 19:20:37 +0100

    push

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,27 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+
+export default function AdminDashboard(){
+    return {
+
+    }
+}
+
+type DashboardCardProps = {
+    title: string
+    subtitle: string
+    body: string
+  }
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+    return (
+      <Card>
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{subtitle}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>{body}</p>
+        </CardContent>
+      </Card>
+    )
+  }
